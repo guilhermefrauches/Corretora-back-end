caac1036db0923bc8c9222ee7d7dd247c989d308
guilhermefrauches <gmoreira2610@gmail.com>
2026-03-15 02:47:43 -0300

    chore: classe principal e teste da aplicação

--- src/main/java/br/com/meuapp/corretorabackend/CorretoraBackEndApplication.java
+++ src/main/java/br/com/meuapp/corretorabackend/CorretoraBackEndApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CorretoraBackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CorretoraBackEndApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CorretoraBackEndApplication.class, args);
+    }
 
 }
