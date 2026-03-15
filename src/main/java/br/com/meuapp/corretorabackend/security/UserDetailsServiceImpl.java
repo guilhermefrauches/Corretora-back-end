f402b224052ac1819f3fc289fa38d522e34f8fac
guilhermefrauches <gmoreira2610@gmail.com>
2026-03-15 02:46:20 -0300

    feat: configuração de segurança JWT

--- src/main/java/br/com/meuapp/corretorabackend/security/UserDetailsServiceImpl.java
+++ src/main/java/br/com/meuapp/corretorabackend/security/UserDetailsServiceImpl.java
@@ -1,4 +1,23 @@
 package br.com.meuapp.corretorabackend.security;
 
-public class UserDetailsServiceImpl {
+import br.com.meuapp.corretorabackend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "Usuário não encontrado com e-mail: " + email));
+    }
 }
