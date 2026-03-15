e8680dae0e343106339638ef3f0948848555c586
guilhermefrauches <gmoreira2610@gmail.com>
2026-03-15 02:46:07 -0300

    feat: criação do UserRepository

--- src/main/java/br/com/meuapp/corretorabackend/repository/UserRepository.java
+++ src/main/java/br/com/meuapp/corretorabackend/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package br.com.meuapp.corretorabackend.repository;
 
-public interface UserRepository {
+import br.com.meuapp.corretorabackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
